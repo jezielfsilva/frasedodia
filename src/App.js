6263ae9150d951b944532cb6fa0b555e45d13459
vainawebm <vainaweb@gmail.com>
2020-06-12 14:35:55 -0300

    parte lógica terminada

--- src/App.js
+++ src/App.js
@@ -10,6 +10,23 @@ const Page = styled.div`
       background: #439;
 `;
 
+const BoxPhrase = styled.div`
+      width: 34vw;
+      height: 30vh;
+      background: #FFFFFF;
+      border-radius: 0.3rem;
+      box-shadow: 2px 2px 2px #242525;
+      display: flex;
+      align-items: center;
+      flex-direction: column;
+      justify-content: space-around;
+`;
+
+const Title = styled.h1`
+      letter-spacing: 0.01em;
+      color: #cf5a0c;
+`;
+
 export default class Screen extends React.Component {
     
     constructor(props) {
@@ -26,21 +43,33 @@ export default class Screen extends React.Component {
       }
     }
 
-    handlePhrase = (frase) => {
+    componentDidMount() {
+      this.handlePhrase()
+    }
+
+    handlePhrase = () => {
+      const valMax = 4
+      const valMin =  0
+      var indexSorted = Math.floor(Math.random() * (valMax - valMin)) + valMin;
       this.setState({
-          select: frase,
+          select: this.state.phrases[indexSorted],
       })
+    }
+
+    share = () => {
+        const sharePhrase = this.state.select
+        window.open (`https://web.whatsapp.com/send?text=${sharePhrase}`)
     }
 
     render () {
       return (
         <Page>
-          <div>
-            <h1>Frase do dia</h1>
-            <p></p>
-            <button>sortear frase</button>
-            <button>compartilhar</button>
-          </div>
+          <BoxPhrase>
+            <Title>Frase do dia</Title>
+            <p>{this.state.select}</p>
+            <button onClick={this.handlePhrase}>sortear frase</button>
+            <button onClick={this.share}>compartilhar</button>
+          </BoxPhrase>
         </Page>
       );
     }
