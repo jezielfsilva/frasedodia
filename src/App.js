6c85a07c04dc9724f0ecd616ee2469464e9ad528
vainawebm <vainaweb@gmail.com>
2020-06-15 10:18:51 -0300

    estilo da página melhorado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import styled from 'styled-components';
+import Logo from './assets/whatsapp-icon-seeklogo.com.svg';
 
 const Page = styled.div`
       height: 100vh;
@@ -25,6 +26,36 @@ const BoxPhrase = styled.div`
 const Title = styled.h1`
       letter-spacing: 0.01em;
       color: #cf5a0c;
+`;
+
+const Text = styled.p`
+      color: #252525;
+      font-size: 1.2rem;
+      letter-spacing: 0.02rem;
+`;
+
+const BoxButtons = styled.div`
+      width: 30vw;
+      height: 6vh;
+      display: flex;
+      align-items: center;
+      justify-content: space-around;
+`;
+
+const Buttons = styled.button`
+      width: 10vw;
+      height: 4vh;
+      background: #252525;
+      color: #FFFFFF;
+      border-style: none;
+      border-radius: 2px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+`;
+
+const LogoWats = styled.img`
+      width: 15%;
 `;
 
 export default class Screen extends React.Component {
@@ -66,9 +97,11 @@ export default class Screen extends React.Component {
         <Page>
           <BoxPhrase>
             <Title>Frase do dia</Title>
-            <p>{this.state.select}</p>
-            <button onClick={this.handlePhrase}>sortear frase</button>
-            <button onClick={this.share}>compartilhar</button>
+            <Text>{this.state.select}</Text>
+            <BoxButtons>
+              <Buttons onClick={this.handlePhrase}>sortear frase</Buttons>
+              <Buttons onClick={this.share}>compartilhar<LogoWats src={Logo}/></Buttons>
+            </BoxButtons>
           </BoxPhrase>
         </Page>
       );
